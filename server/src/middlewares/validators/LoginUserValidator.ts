8f0ceb0360f3c208d04c50e8bc13b0f49c2159bf
kandy <kandypeter03@gmail.com>
2023-01-20 11:35:21 +0200

    Implement the gallery controller

--- server/src/middlewares/validators/LoginUserValidator.ts
+++ server/src/middlewares/validators/LoginUserValidator.ts
@@ -1,46 +1,34 @@
-import { Request, Response, NextFunction } from 'express';
-import joi from 'joi';
-import {LoginUserValidationInterface,LoginUserResponseInterface } 
-from '../../interfaces/validation/LoginUserInterface';
-import ValidationException from '../CustomErrorException/ValidationExceptionHandler';
+import { Request, Response, NextFunction } from "express";
+import joi from "joi";
+import {
+  LoginUserValidationInterface,
+  LoginUserResponseInterface,
+} from "../../interfaces/validation/LoginUserInterface";
+import ValidationException from "../CustomErrorException/ValidationExceptionHandler";
 
+const ValidationRules = (requestBody: LoginUserValidationInterface) => {
+  const schema: joi.ObjectSchema = joi.object({
+    email: joi.string().trim().required().email(),
+    password: joi.string().trim().min(8).required(),
+  });
 
+  const reqValidate = {
+    email: requestBody?.email,
+    password: requestBody?.password,
+  };
 
+  const responseValidation: any = schema.validate(reqValidate);
 
-
- const ValidationRules = (requestBody:LoginUserValidationInterface) => 
- {
-     const schema:joi.ObjectSchema = joi.object({
-         email: joi.string().trim().required().email(),
-         password: joi.string().trim().min(8).required(),
-       });
-
-       const reqValidate = {
-         email:requestBody?.email,
-         password:requestBody?.password
-       }
-    
-      const responseValidation:any = schema.validate(reqValidate);
-
-      if(responseValidation.hasOwnProperty('error'))
-      {
-         return {
-           errorStatus:true, 
-           error: responseValidation?.error?.details[0]?.message
-         }
-      }
-      
-       return {
-         errorStatus:false
-       };
-  
-   }
- 
-
-
-  
-
-
-  export {
-    ValidationRules as LoginUserValidation
+  if (responseValidation.hasOwnProperty("error")) {
+    return {
+      errorStatus: true,
+      error: responseValidation?.error?.details[0]?.message,
+    };
   }
+
+  return {
+    errorStatus: false,
+  };
+};
+
+export { ValidationRules as LoginUserValidation };
