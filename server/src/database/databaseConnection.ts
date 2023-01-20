c3364acc8deb5c024cb801cd4c6bc534f34f6e4e
kandy <kandypeter03@gmail.com>
2023-01-20 10:22:50 +0200

    Setup db and mits models

---
+++ server/src/database/databaseConnection.ts
@@ -0,0 +1,20 @@
+import {Connection, createConnection} from "typeorm";
+
+
+export const connection = async() =>
+{
+    return await createConnection(
+              {
+                  type: "postgres",
+                  host: process.env.DATABASE_HOST + "",
+                  port: Number(process.env.DATABASE_PORT),
+                  username: process.env.DATABASE_USERNAME + "",
+                  password: process.env.DATABASE_PASSWORD + "",
+                  database: process.env.DATABASE_NAME + "",
+                  entities: ["src/database/entities/**/*.ts"],
+                  synchronize: true,
+                  name: 'default'
+              },
+          );
+}
+
