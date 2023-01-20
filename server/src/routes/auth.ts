f0a604ac55ad04d429ea9359b54506141f1c445f
kandy <kandypeter03@gmail.com>
2023-01-20 10:34:14 +0200

    Implement authentication routes

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+import { Router } from "express";
+import { AuthController } from "../controllers/UserController";;
+import {container} from 'tsyringe';
+
+const Auth = container.resolve(AuthController);
+
+
+const router = Router();    
+//Login route
+router.post("/login", Auth.Login);
+router.post("/register", Auth.Register);
+
+
+export default router;
+
+ 
