1dfbe7c3e7a74fe8bc2a86f44421adfaf2776e56
kandy <kandypeter03@gmail.com>
2023-01-20 11:39:54 +0200

    Setup gallery routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from "express";
+import { checkJwt } from "../middlewares/jwt/checkjwt";
 import auth from "./auth";
+import gallery from './gallery'
 
 const routes = Router();
 
 routes.use("/auth",  auth);
+routes.use("/gallery", [checkJwt], gallery);
 
 export default routes;
